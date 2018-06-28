844901e4255794617c0cdfae80df6ecfa8f8e404
a8568730 <a8568730@hotmail.com>
2018-06-28 15:25:09 +0800

    頁kha 加上其他網站 kah MIT授權

--- src/元件/頁腳.js
+++ src/元件/頁腳.js
@@ -7,12 +7,20 @@ const sites = [
     url: "https://github.com/i3thuan5/lo5ma2ji7_tsuan2huan7",
   },
   {
+    title: "臺語自動斷詞器",
+    url: "https://xn--hhr625bzsunxh.xn--v0qr21b.xn--kpry57d/",
+  },
+  {
+    title: "連字符書寫檢查器",
+    url: "https://xn--p8s050ac6bg7zsxy.xn--v0qr21b.xn--kpry57d/",
+  },
+  {
     title: "詞彙分級網站",
     url: "https://xn--kbrr83a6ixbzl.xn--v0qr21b.xn--kpry57d/",
   },
   {
-    title: "意傳文化科技",
-    url: "https://xn--v0qr21b.xn--kpry57d/",
+    title: "台中教育大學台語系 MIT授權",
+    url: "http://taiwanese.ntcu.edu.tw/",
   },];
 
 const Iah8Kha1 = () =>
