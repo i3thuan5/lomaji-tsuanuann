07c0376600dfdde962a3d6ab14a64f4594b1a0a2
Fafofafoy <ihcaoe@gmail.com>
2021-07-19 11:21:29 +0800

    更新網址。

--- src/元件/頁腳.js
+++ src/元件/頁腳.js
@@ -7,22 +7,6 @@ const sites = [
     url: "https://github.com/i3thuan5/lomaji-tsuanuann",
   },
   {
-    title: "臺語自動斷詞器",
-    url: "https://xn--hhr625bzsunxh.xn--v0qr21b.xn--kpry57d/",
-  },
-  {
-    title: "連字符書寫檢查器",
-    url: "https://xn--p8s050ac6bg7zsxy.xn--v0qr21b.xn--kpry57d/",
-  },
-  {
-    title: "詞彙分級網站",
-    url: "https://xn--kbrr83a6ixbzl.xn--v0qr21b.xn--kpry57d/",
-  },
-  {
-    title: "台中教育大學台語系 MIT開源授權",
-    url: "http://taiwanese.ntcu.edu.tw/",
-  },
-  {
     title: "意傳科技",
     url: "https://ithuan.tw/",
   },];
